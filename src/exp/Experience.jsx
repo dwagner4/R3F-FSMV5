ebd7ccc21cfd8e7db0c5e6f6763c0edacfbb6a16
Dean Wagner <dean@dfwagners.net>
2023-12-22 18:45:58 +0000

    added Sphere

--- src/exp/Experience.jsx
+++ src/exp/Experience.jsx
@@ -1,5 +1,6 @@
 import { OrbitControls } from "@react-three/drei";
 import { Cube } from '../models/Cube.jsx'
+import { Sphere } from '../models/Sphere.jsx'
 
 export const Experience = () => {
   return (
@@ -7,6 +8,9 @@ export const Experience = () => {
       <OrbitControls />
       <Cube position={[ -1, 0, 1]} tag="Joe" fsm="shapeRef1" friend="shapeRef2"/>
       <Cube position={[ 1, 0, -1]} tag ="fart" fsm="shapeRef2" friend="shapeRef1"/>
+      <Sphere position={[ 1, 0, 1]} scale={0.5}/>
+      <directionalLight color="red" position={[0, 5, 5]} intensity={2} />
+      <ambientLight color="blue" intensity={0.5} />
     </>
   );
 };
